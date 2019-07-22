d19ce09d984f82c4ef7b8591c5e7906258f0444d
ybejgamwar <y.bejgamwar@gmail.com>
2019-07-22 15:53:51 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 provider "aws" {
-  access_key = "${var.ACCESS_KEY}"
-  secret_key = "${var.SECRET_KEY}"
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
   region = "eu-west-1"
 }
 terraform {
